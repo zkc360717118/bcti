91f00e5dab177f6724e8681dba2cd2cb0219d76d
kevin <kevin_zkc@126.com>
2016-12-20 21:08:06 +0800

    'hah'

---
+++ resources/views/quote/maketour.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<!-- 新 Bootstrap 核心 CSS 文件 -->
+<link href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="http://apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script>
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<body>
+
+<form action="" method="post">
+    <div class="containner">
+        <div class="row">
+            <div class="col-sm-2" style="background: red">01-11-2016</div>
+            <div class="col-sm-2" style="background: black">D</div>
+            <div class="col-sm-4" style="background: blue">BEIJING/SHANGHAI</div>
+            <input type="text" class="2">
+        </div>
+        <div class="row" style="margin-bottom: 20px;">
+            <textarea class="col-sm-6">pick up from airport , transfer to hotel.</textarea>
+            <input type="text" class="col-sm-2" >
+            <input type="text" class="col-sm-2" >
+        </div>
+        <div class="row">
+            <div class="col-sm-2" style="background: red">01-11-2016</div>
+            <div class="col-sm-2" style="background: black">D</div>
+            <div class="col-sm-4" style="background: blue">BEIJING/SHANGHAI</div>
+            <input type="text" class="2">
+        </div>
+        <div class="row" style="margin-bottom: 20px;">
+            <textarea class="col-sm-6">take a walk at bund,enjoy the outside view of TV TOWER,Century Square,shopping at  Silk Factory, leisure at the XinTianDi bar Street .
+            </textarea>
+            <input type="text" class="col-sm-2" >
+            <input type="text" class="col-sm-2" >
+        </div>
+        <div class="row">
+            <div class="col-sm-2" style="background: red">01-11-2016</div>
+            <div class="col-sm-2" style="background: black">D</div>
+            <div class="col-sm-4" style="background: blue">BEIJING/SHANGHAI</div>
+            <input type="text" class="2">
+        </div>
+        <div class="row" style="margin-bottom: 20px;">
+            <textarea class="col-sm-6">pick up from airport , transfer to hotel.</textarea>
+            <input type="text" class="col-sm-2" >
+            <input type="text" class="col-sm-2" >
+        </div>
+    </div>
+
+</form>
+</body>
+</html>
